15a1598568b9c664873ca95f80546b6ddc4a1472
Svhodro <rudrolipi@gmail.com>
2024-05-13 01:57:28 +0600

    cors fix

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: [      
-      "https://ass-11-iota.vercel.app", 
-      "https://ass-11-iota.vercel.app",
-    ],
+    origin: "*",
     credentials: true,
   })
 );
