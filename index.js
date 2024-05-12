f42934ef9f5e91b490b6a3823c1548147f174cfd
Svhodro <rudrolipi@gmail.com>
2024-05-13 02:02:08 +0600

    cors fix

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = 5000;
@@ -7,8 +6,6 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // middleware
 app.use(cors());
 app.use(express.json());
-
-app.use(cors());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://yoursite.com");
   res.header(
