592ff7c67fbba3062944d8427ae7d573acc2fcc4
Svhodro <rudrolipi@gmail.com>
2024-05-13 14:12:13 +0600

    entry mahakal

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cors = require('cors');
+// const cors = require('cors');
 require("dotenv").config();
 const app = express();
 const port = 5000;
@@ -11,11 +11,11 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
 });
-app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-});
+// app.use(cors());
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   next();
+// });
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@ass-11.c2lx29d.mongodb.net/?retryWrites=true&w=majority&appName=Ass-11`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
