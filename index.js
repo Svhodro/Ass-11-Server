d4074209d52e29b594ca6bfe8157eaaafb83cbd4
Svhodro <rudrolipi@gmail.com>
2024-05-13 14:38:31 +0600

    entry mahakal

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, mode'); // Add this line
+  // Other headers...
   next();
 });
 // app.use(cors());
